4deff474cb168d145baa976425b2311ab673b6ec
eyungi <dbsrl0221@gmail.com>
2025-11-07 02:44:17 +0900

    prj_2

---
+++ prj_2/src/examples/additional.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <syscall.h>
+
+int main(int argc, char *argv[])
+{
+    int fibo = fibonacci(atoi(argv[1]));
+    printf("%d ", fibo);
+    int m = max_of_four_int(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]), atoi(argv[4]));
+    printf("%d\n",m);
+    return EXIT_SUCCESS;
+}
